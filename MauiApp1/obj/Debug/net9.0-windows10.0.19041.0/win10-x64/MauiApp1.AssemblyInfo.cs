d8546e4b093a613c12141b7b46f225a1a8d0d213
Mager0117 <mager0117@example.com>
2025-03-28 19:55:02 +0700

    Update all files

--- MauiApp1/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d48aee1ec3748a4274a0cd67a6dd5ce162efea49")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
